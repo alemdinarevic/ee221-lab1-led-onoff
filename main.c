d61faf5d23b03023982cbc25d08885b885a6e238
Alem Dinarević <alemdinarevic@gmail.com>
2020-03-27 19:15:28 +0100

    different way added

--- main.c
+++ main.c
@@ -9,18 +9,27 @@ int led = 13; // Pin 13
      Serial.flush();
  }
  
+//simultaneous on off
+// void loop()
+// {
+//      char input;
+//  // Read any serial input
+//      while (Serial.available() > 0)
+//      {
+//          input = Serial.read(); // Read in one char at a time
+//          if (input == '1')
+//             digitalWrite(led, HIGH); // on
+//          if (input == '0')
+//             digitalWrite(led, LOW); // off
+//      }
+    
+// }
+
+//simultaneous on off in a different way
 void loop()
 {
-     char input;
- // Read any serial input
-     while (Serial.available() > 0)
-     {
-         input = Serial.read(); // Read in one char at a time
-         if (input == '1')
-            digitalWrite(led, HIGH); // on
-         if (input == '0')
-            digitalWrite(led, LOW); // off
-     }
-    
+ digitalWrite(led, HIGH);
+ delay(1000);
+ digitalWrite(led, LOW);
+ delay(1000);
 }
-
